b4f319075feeb3685205db968733dce22754fd27
Craig Schlegelmilch <cschlegelmilch@me.com>
2022-02-28 17:04:23 -0700

    Specify port

--- runtime.py
+++ runtime.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +11,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=os.getenv("PORT", 5000))
