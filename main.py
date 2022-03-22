b640f57a08179bff545ff341e197c948e843f1cb
Craig Schlegelmilch <cschlegelmilch@me.com>
2022-03-22 13:04:49 -0600

    Fixed numberic detection

--- main.py
+++ main.py
@@ -9,18 +9,22 @@ from pd_anonymizer.anonymizers import KAnonymizer
 
 
 def anonymize(df, qids):
+    print("Running PD anonymize....")
     # Figure out numeric column names
     numeric_names = ['age', 'zip', 'zipcode', 'year', 'dob']
     numeric_names_regex = '^(' + '|'.join(numeric_names) + ')$'
-    num_columns = []
-
-    for colname, series in df.items():
-        if re.match(numeric_names_regex, colname, flags=re.IGNORECASE) and (
-                series.dtype in [int, float, complex] or types.is_numeric_dtype(series.dtype)):
-            num_columns.append(colname)
+    num_columns = [
+        colname for colname, series in df.items() if (
+                colname in qids
+                and re.match(numeric_names_regex, colname, flags=re.IGNORECASE)
+                and (
+                        series.dtype in [int, float, complex]
+                        or types.is_numeric_dtype(series.dtype)
+                )
+        )
+    ]
 
     cat_columns = list(set(qids) - set(num_columns))
-
     k_target = 30
     test_hdf = HashableDataFrame(df)
 
